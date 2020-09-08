757825babbe1c583a700a9c5e7e593bfd7a81ab3
KalluruS <>
2020-09-08 08:34:52 +0530

    Update shell-scripting/

--- 03-variables.sh
+++ 03-variables.sh
@@ -18,7 +18,7 @@ echo $a
 # Some times you may need to store multiple values in a single variable.
 # In shell we call it as array, In some scripting languages this is called as a list.
 
-ARRAY=(1,2,abc,20,0.0.1)
+ARRAY=(1 2 abc 20 0.0.1)
 
 # Single Array can hold multiple daata types, Ofcourse in shell everything is a string.
 
