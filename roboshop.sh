e75ac957561003f0193f834922ef8f6fb149cd07
KalluruS <>
2020-09-09 07:57:25 +0530

    Update shell-scripting/

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+
+case $USER_ID in
+  0)
+    echo "Starting Installation"
+  ;;
+  *)
+    echo "You should be a root user to perform this script"
+    ;;
+esac
+
 case $1 in
   frontend)
     echo Installing Frontend
-    yum install nginx -y 
+    yum install nginx -y
     ;;
   catalogue)
     echo Installing Catalogue
