d2b90bd98b4b2f8a452dbdf8a4d86682d7fda6dc
KalluruS <>
2020-09-09 08:26:07 +0530

    Update shell-scripting/

---
+++ 06-functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# If you name to data is called a variable
+# If you name to commands is called as a function.
+
+
+## declare function
+
+function abc() {
+  ## Commands
+  echo Hello from abc
+}
+
+xyz() {
+  echo Hello from xyz
+}
+
+
+## Main Program
+abc
+xyz
