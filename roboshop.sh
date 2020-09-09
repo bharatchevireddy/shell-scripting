fc26ba7fabdcb915a09ff68f46522c1db0f20a82
KalluruS <>
2020-09-09 07:47:05 +0530

    Update shell-scripting/

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo Installing Frontend
-echo Installing Catalogue
-echo Installing Cart
+case $1 in
+  frontend)
+    echo Installing Frontend
+    echo Completed Installing Frontend
+    ;;
+  catalogue)
+    echo Installing Catalogue
+    echo Completed Installing  Catalogue
+    ;;
+  cart)
+    echo Installing Cart
+    echo Completed Installing Cart
+    ;;
+esac
