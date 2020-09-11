2b2f3c8b0fbf0d3e5a0326d37bb4bdbab0cbddd8
KalluruS <>
2020-09-11 08:21:42 +0530

    Update shell-scripting/

--- roboshop.sh
+++ roboshop.sh
@@ -87,6 +87,7 @@ case $1 in
     rm -rf static README.md
     mv localhost.conf /etc/nginx/nginx.conf
     sed -i -e '/^#/ d' /etc/nginx/nginx.conf
+    sed -i -e "/localhost/ a \tlocation /test { \n\t hello \n}"
     Print "Starting Nginx"
     systemctl enable nginx
     systemctl start nginx
